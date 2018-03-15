25920aec7975e7224e5e76601ab25ed44d7f50c9
shubhamguptaorg <shubhamguptaorg@gmail.com>
2018-03-15 19:29:50 +0530

    add static file ->header.html

--- free_ebooks/views.py
+++ free_ebooks/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>this is the index of the free ebooks</h1>")
+    return render(request,'free_ebooks/header.html',{})
