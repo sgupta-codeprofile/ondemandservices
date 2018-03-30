75a670a42244fdcc4cd4eb3483cc68ca9ff25d3c
shubhamguptaorg <shubhamguptaorg@gmail.com>
2018-03-30 14:58:17 +0530

    Fix extension error and do minor changes

--- image_resizer/views.py
+++ image_resizer/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from image_resizer.forms import upload_image
 from PIL import Image
 import webbrowser
-# Create your views here.
 def index(request):
     if request.user.is_authenticated:
         if request.method == 'POST':
@@ -13,10 +12,15 @@ def index(request):
                 catch_width=request.POST['width']
                 catch_height=request.POST['height']
                 ext = ".jpg"
-                temp_image=Image.open(catch_image)
-                resize_catch =temp_image.resize((int(catch_width), int(catch_height)), Image.ANTIALIAS)
-                resize_catch.save("ANTIALIAS" + ext)
-                return HttpResponse(webbrowser.open('file:///F:/major_project/mp/ondemandservices/ANTIALIAS.jpg'))
+                try:
+                    temp_image=Image.open(catch_image)
+                    resize_catch =temp_image.resize((int(catch_width), int(catch_height)), Image.ANTIALIAS)
+                    resize_catch.save("ANTIALIAS" + ext)
+                    webbrowser.open('file:///F:/major_project/mp/ondemandservices/ANTIALIAS.jpg')
+                    return render(request,'image_resizer/resize_success.html',{})
+                except:
+                    minorcheck=10
+                    return render(request,'image_resizer/index.html',{'minorcheck':minorcheck,'form':upload_image})
             else:
                 flag=1
                 return render(request,'image_resizer/index.html',{'flag_for_form_validation':flag,'form':upload_image})
