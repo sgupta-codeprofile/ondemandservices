f6902b7b9309d1a66cfb6109e70a026ebcccfcbf
shubhamguptaorg <shubhamguptaorg@gmail.com>
2018-03-30 15:06:59 +0530

    Blank commit start work on resume maker

--- resume_maker/views.py
+++ resume_maker/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>This is the index of resume maker")
+    return render(request,'resume_maker/index.html',{})
