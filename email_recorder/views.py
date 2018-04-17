36e7794e1523c2423f9bf9d2d7d7c686ad83aea7
shubhamguptaorg <shubhamguptaorg@gmail.com>
2018-04-18 03:58:06 +0530

    fix minor error in email recorder app

--- email_recorder/views.py
+++ email_recorder/views.py
@@ -4,26 +4,39 @@ from email_recorder.forms import catagories,download_list
 from email_recorder.models import cat,liststore,email
 from django.core.exceptions import ObjectDoesNotExist
 
-
 def index(request):
     if request.user.is_authenticated:
         if request.method=='POST':
             received_data=catagories(request.POST)
             if received_data.is_valid():
-                start_load=email()
-                start_load.usr_email=request.user.email
-                start_load.save()
                 try:
-                    get_usr=email.objects.get(pk=request.user.email)
-                    get_usr.cat_set.create(test=request.POST['cat_input'])
-                    get_usr.save()
-                    get_cat=cat.objects.get(test=request.POST['cat_input'])
-                    get_cat.liststore_set.create(heavylist=request.POST['user_email'])
-                    get_cat.save()
-                except:
-                    return HttpResponse("exception run report is sumbitted to admin")
-                return HttpResponse('success')
+                    get_usr = email.objects.get(pk=request.user.email)
+                    get_data = request.POST['cat_input']
+                    if (get_data == 'Job_seekers'):
+                        s = email.objects.get(pk=request.user.email)
+                        job_load = s.cat_set.get(test='Job_Seekers')
+                        job_load.liststore_set.create(heavylist=request.POST['user_email'])
+                        return HttpResponse('success added')
+                    elif(get_data=='Earn_money'):
+                        s = email.objects.get(pk=request.user.email)
+                        job_load = s.cat_set.get(test='Earn_Money')
+                        job_load.liststore_set.create(heavylist=request.POST['user_email'])
+                        return HttpResponse('success added')
+                    else:
+                        return HttpResponse('fail')
+                except ObjectDoesNotExist:
+                    start_load = email()
+                    start_load.usr_email = request.user.email
+                    start_load.save()
+                    start_load_categories = email.objects.get(pk=request.user.email)
+                    start_load_categories.cat_set.create(test='Job_Seekers')
+                    start_load_categories.cat_set.create(test='Earn_Money')
+                    start_load_categories.cat_set.create(test='Technology')
+                    start_load_categories.cat_set.create(test='Political')
+                    start_load_categories.cat_set.create(test='Crypto_currency')
+                    return HttpResponse('You created your own directory now you can save the email')
             else:
+
                 return HttpResponse("eroor in database")
 
         else:
