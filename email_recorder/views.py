d3475223ee6141cdb6216c6bce0cc19922bc3a2d
shubhamguptaorg <shubhamguptaorg@gmail.com>
2018-04-18 04:28:44 +0530

    add all field and configure it with unit testing

--- email_recorder/views.py
+++ email_recorder/views.py
@@ -16,19 +16,34 @@ def index(request):
                         s = email.objects.get(pk=request.user.email)
                         job_load = s.cat_set.get(test='Job_Seekers')
                         job_load.liststore_set.create(heavylist=request.POST['user_email'])
-                        return HttpResponse('success added')
+                        return render(request,'email_recorder/emailsendsuccessfully.html',{})
                     elif(get_data=='Earn_money'):
                         s = email.objects.get(pk=request.user.email)
                         job_load = s.cat_set.get(test='Earn_Money')
                         job_load.liststore_set.create(heavylist=request.POST['user_email'])
-                        return HttpResponse('success added')
+                        return render(request,'email_recorder/emailsendsuccessfully.html',{})
                     elif(get_data=='Technology_lover'):
                         s = email.objects.get(pk=request.user.email)
                         job_load = s.cat_set.get(test='Technology')
                         job_load.liststore_set.create(heavylist=request.POST['user_email'])
-                        return HttpResponse('success added')
+                        return render(request,'email_recorder/emailsendsuccessfully.html',{})
+                    elif(get_data=='Gadgets_lover'):
+                        s = email.objects.get(pk=request.user.email)
+                        job_load = s.cat_set.get(test='Gadgets_lover')
+                        job_load.liststore_set.create(heavylist=request.POST['user_email'])
+                        return render(request,'email_recorder/emailsendsuccessfully.html',{})
+                    elif(get_data=='Political'):
+                        s = email.objects.get(pk=request.user.email)
+                        job_load = s.cat_set.get(test='Political')
+                        job_load.liststore_set.create(heavylist=request.POST['user_email'])
+                        return render(request,'email_recorder/emailsendsuccessfully.html',{})
+                    elif(get_data=='Crypto_currency'):
+                        s = email.objects.get(pk=request.user.email)
+                        job_load = s.cat_set.get(test='Crypto_currency')
+                        job_load.liststore_set.create(heavylist=request.POST['user_email'])
+                        return render(request,'email_recorder/emailsendsuccessfully.html',{})
                     else:
-                        return HttpResponse('fail')
+                        return render(request,'bulk_email_sender/somethingwentwrong.html',{})
                 except ObjectDoesNotExist:
                     start_load = email()
                     start_load.usr_email = request.user.email
@@ -37,15 +52,16 @@ def index(request):
                     start_load_categories.cat_set.create(test='Job_Seekers')
                     start_load_categories.cat_set.create(test='Earn_Money')
                     start_load_categories.cat_set.create(test='Technology')
+                    start_load_categories.cat_set.create(test='Gadgets_lover')
                     start_load_categories.cat_set.create(test='Political')
                     start_load_categories.cat_set.create(test='Crypto_currency')
-                    return HttpResponse('You created your own directory now you can save the email')
+                    return render(request,'email_recorder/startadd.html',{})
             else:
 
-                return HttpResponse("eroor in database")
+                return HttpResponse("error in database")
 
         else:
-            return render(request, 'email_recorder/index.html', {'form': catagories(), 'success_urna': 1})
+            return render(request, 'email_recorder/index.html', {'form': catagories()})
     else:
         return render(request,'free_ebooks/signinfirst.html',{})
 
